1b05fb621b89d26f7d2ee2c697da7396dcca2a5a
Milan Matějček <milan.matejcek@gmail.com>
2026-09-25 11:47:07 +0200

    refactor: replace class_alias traits with deprecated traits

---
+++ src/MemoryStorage.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace h4kuna\Memoize;
+
+/**
+ * @deprecated use Memoize
+ * @see Memoize
+ */
+trait MemoryStorage
+{
+
+	use Memoize;
+
+}
---
+++ src/MemoryStorageStatic.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace h4kuna\Memoize;
+
+/**
+ * @deprecated use MemoizeStatic
+ * @see MemoizeStatic
+ */
+trait MemoryStorageStatic
+{
+
+	use MemoizeStatic;
+
+}
